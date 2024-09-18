8d2003c4f9435e373e74a75b84e2f75c74267341
Arancha Fernandez <aferfer1978@gmail.com>
2024-09-18 18:07:28 +0200

    first commit

---
+++ Hello.java
@@ -0,0 +1,96 @@
+//import java.util.Scanner;
+//import javax.swing.JOptionPane;
+
+public class Hello {
+    public static void main(String[] args) {
+        // Declare variables
+        // Declarar una variable 
+        // int x;
+        // Asignación de una variable
+        // x = 33;
+        // Inicializiación de una variable
+        // int x = 33;
+
+        /* int x = 78;
+        byte a = 122;
+        short m = 32766;
+        long e = 132846793824567L;
+        float b = 3.18778f;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Arancha";
+
+        System.out.println(x);
+        System.out.println(a);
+        System.out.println(e);
+        System.out.println(b);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(symbol);
+        System.out.println("Hola mi nombre es: " + name); */
+
+        //Swap two variables
+        /*String x = "agua";
+        String y = "coca-cola";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " +y); */
+
+        // User input
+        /* Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What is your favorite food? ");
+        String food = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("You like " + food);
+
+        scanner.close(); */
+
+        // Espresiones 
+        // Expression = operands & operators
+        // Operands = values, variables, numbers, quatity
+        // operators = + - * / %
+        /* int friends = 10;
+        friends = friends + 1;
+        friends = friends - 1;
+        friends = friends * 1;
+        friends = friends / 1;
+        friends = friends % 1;
+
+        // so that it can store  doubles you would need to change int for double
+        // double friends = 10;
+        // friends = (double) friends / 3;
+        // System.out.println(friends); will return 3.333333333
+
+        friends++;
+        friends--;
+        System.out.println(friends); */
+
+        // GUI Intro = Graphical User Interface
+        /* String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null,"Hello " + name); 
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null,"You are  " + age + " years old"); 
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null,"You are  " + height + " cm tall");  */
+
+        
+    }
+}
